10b198cf71c937c1639011a4ef54d81e3e74addf
vavikast <vavikast@gmail.com>
2018-10-24 07:45:54 +0800

    felix upate

--- felix/views.py
+++ felix/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 
 # Create your views here.
 def index(request):
-    pass
+    return render(request, 'felix/index.html')
+
+
+def login(request):
+    return render(request, 'felix/login.html')
+
+
+def register(request):
+    return render(request, 'felix/register.html')
+
+
+def logout(request):
+    return redirect("/")
--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from felix import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('felix/', include('felix.urls'))
+    path('', views.index, name='index'),
+    path('login/', views.login, name='login'),
+    path('register/', views.register,name='register'),
+    path('logout/', views.logout, name='logout'),
 ]
