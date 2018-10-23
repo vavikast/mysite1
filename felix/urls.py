449545d7c642d3649506a39373ef86ef9f7386a3
vavikast <vavikast@gmail.com>
2018-10-23 09:40:22 +0800

    mysite

---
+++ felix/urls.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+2018 - 10 - 23
+__author__ = 'Felix'
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index')
+]
