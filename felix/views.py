c8ea5a6adec4d847a4322ddf8058f01e0bf1d82c
vavikast <vavikast@gmail.com>
2018-10-31 07:46:46 +0800

    主页面修改

--- felix/views.py
+++ felix/views.py
@@ -2,6 +2,14 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from . import models
 from . import forms
+import hashlib
+
+def hash_code(s, salt='mysite'):# 加点盐
+    h = hashlib.sha256()
+    s += salt
+    h.update(s.encode())  # update方法只接收bytes类型
+    return h.hexdigest()
+
 
 # Create your views here.
 def index(request):
@@ -9,6 +17,8 @@ def index(request):
 
 
 def login(request):
+    if request.session.get('is_login',None):
+        return redirect("/")
     if request.method == "POST":
         login_form = forms.UserForm(request.POST)
         message = "请检查填写的内容！"
@@ -18,6 +28,9 @@ def login(request):
             try:
                 user = models.User.objects.get(name=username)
                 if user.password == password:
+                    request.session['is_login'] = True
+                    request.session['user_id'] = user.id
+                    request.session['user_name'] = user.name
                     return redirect('')
                 else:
                     message = "密码不正确！"
@@ -31,8 +44,46 @@ def login(request):
 
 
 def register(request):
-    return render(request, 'felix/register.html')
+    if request.session.get('is_login', None):
+        # 登录状态不允许注册。你可以修改这条原则！
+        return redirect("/")
+    if request.method == "POST":
+        register_form = forms.RegisterForm(request.POST)
+        message = "请检查填写的内容！"
+        if register_form.is_valid():  # 获取数据
+            username = register_form.cleaned_data['username']
+            password1 = register_form.cleaned_data['password1']
+            password2 = register_form.cleaned_data['password2']
+            email = register_form.cleaned_data['email']
+            sex = register_form.cleaned_data['sex']
+            if password1 != password2:  # 判断两次密码是否相同
+                message = "两次输入的密码不同！"
+                return render(request, 'felix/register.html', locals())
+            else:
+                same_name_user = models.User.objects.filter(name=username)
+                if same_name_user:  # 用户名唯一
+                    message = '用户已经存在，请重新选择用户名！'
+                    return render(request, 'felix/register.html', locals())
+                same_email_user = models.User.objects.filter(email=email)
+                if same_email_user:  # 邮箱地址唯一
+                    message = '该邮箱地址已被注册，请使用别的邮箱！'
+                    return render(request, 'felix/register.html', locals())
+
+                # 当一切都OK的情况下，创建新用户
+
+                new_user = models.User()
+                new_user.name = username
+                new_user.password = hash_code(password1)
+                new_user.email = email
+                new_user.sex = sex
+                new_user.save()
+                return redirect('/')  # 自动跳转到登录页面
+    register_form = forms.RegisterForm()
+    return render(request, 'felix/register.html', locals())
 
 
 def logout(request):
+    if not request.session.get('is_login', None):
+        return redirect("/")
+    request.session.flush()
     return redirect("/")
--- mysite/urls.py
+++ mysite/urls.py
@@ -22,5 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
+    path('logout/', views.logout, name='logout'),
     path('captcha',include('captcha.urls')),
 ]
