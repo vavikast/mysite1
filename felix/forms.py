ea5ee81b75cec340bfa6dd498bc65a7797d254ab
vavikast <vavikast@gmail.com>
2018-10-30 23:19:40 +0800

    主页面修改

--- felix/forms.py
+++ felix/forms.py
@@ -3,7 +3,9 @@
 __author__ = 'Felix'
 
 from  django import forms
+from captcha.fields import CaptchaField
 
 class UserForm(forms.Form):
     username = forms.CharField(label="用户名", max_length=128)
     password = forms.CharField(label="密码", max_length=256, widget=forms.PasswordInput)
+    captcha = CaptchaField(label='验证码')
--- mysite/urls.py
+++ mysite/urls.py
@@ -17,10 +17,10 @@ from django.contrib import admin
 from django.urls import path,include
 from felix import views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('login/', views.login, name='login'),
-    path('register/', views.register,name='register'),
-    path('logout/', views.logout, name='logout'),
+    path('captcha',include('captcha.urls')),
 ]
